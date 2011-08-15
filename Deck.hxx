4e35d2dd1476641ba91c3da42c175155365cd654
Matt Renaud <mrenaud92@gmail.com>
2011-08-15 10:47:43 -0400

    Added dealHand<unsigned> function and a test file for Deck.hxx

--- Deck.hxx
+++ Deck.hxx
@@ -9,73 +9,90 @@
 class Deck
 {
 protected:
-   // Some card deck constants.
-   static const int CARDS_IN_DECK = 52;
-   static const int SUITS_IN_DECK = 4;
-   static const int CARDS_IN_SUIT = 13;
+  // Some card deck constants.
+  static const int CARDS_IN_DECK = 52;
+  static const int SUITS_IN_DECK = 4;
+  static const int CARDS_IN_SUIT = 13;
 
-   unsigned positionInDeck;
-   std::array<spc_type,CARDS_IN_DECK> deck;
+  unsigned positionInDeck;
+  std::array<spc_type,CARDS_IN_DECK> deck;
 
 public:
-   Deck() : positionInDeck(0)  // Deck constructor.
-   {
-      int c = 0;
-      for(int s = 0; s < 4; ++s)
-	 for(int r = 1; r <= 13; ++r)
-	 {
-	    deck[c++] = spc_type (new Card(Rank(r), Suit(s)));
-	    //deck[c].reset();
-	 }
-   }
+  Deck() : positionInDeck(0)  // Deck constructor.
+  {
+    unsigned c = 0;
+    for(unsigned s = 0; s < 4; ++s)
+      for(unsigned r = 2; r <= 14; ++r)
+      {
+	deck[c++] = spc_type (new Card(Rank(r), Suit(s)));
+	//deck[c].reset();
+      }
+  }
 
-   void swap(int i, int j)
-   {
-      deck[i].swap(deck[j]);
-   }
+  void swap(unsigned i, unsigned j)
+  {
+    deck[i].swap(deck[j]);
+  }
 
-   void showDeck()
-   {
-      // for_each using lambda function.
-      /*
+  void showDeck()
+  {
+    // for_each using lambda function.
+    /*
       for_each(
-	 deck, deck+CARDS_IN_DECK,
-	 [](spc_type& c) { if (c) c->showCard(); cout << endl; }
+      deck, deck+CARDS_IN_DECK,
+      [](spc_type& c) { if (c) c->showCard(); cout << endl; }
 //	 [](auto c) { if (c) c->showCard(); cout << endl; }
-      );
-      */
+);
+    */
 
-      // Range based for loop.
-      for(auto &c: deck)
-      {
-	 c->showCard();
-	 std::cout << std::endl;
-      }
-   }
+    // Range based for loop.
+    for(spc_type &c: deck)
+    {
+      std::cout << c;
+      std::cout << std::endl;
+    }
+  }
 
-   void shuffle()
-   {
-      std::tr1::mt19937 gen(std::time(0));
-      std::tr1::uniform_real<> dist(1,51);
-      std::tr1::variate_generator< mt19937&, uniform_real<> > rnd(gen, dist);
+  void shuffle()
+  {
+    using std::tr1::mt19937;
+    using std::tr1::uniform_int;
+    using std::tr1::variate_generator;
 
-      int pos;
-      for(int i = 0; i < CARDS_IN_DECK; ++i)
-      {
-	 pos = rnd();
-	 spc_type c = deck[pos];
-	 deck[pos] = deck[i];
-	 deck[i] = c;
-      }
-   }
+    mt19937 gen(std::time(0));
+    uniform_int<> dist(1,51);
+    variate_generator<mt19937&, uniform_int<> > rnd(gen, dist);
 
-   spc_type dealTopCard()
-   {
-      if(positionInDeck < CARDS_IN_DECK)
-	 return deck[positionInDeck++];
-      else
-	 return deck[0];
-   }
+    int pos;
+    for(unsigned i = 0; i < CARDS_IN_DECK; ++i)
+    {
+      pos = rnd();
+      spc_type c = deck[pos];
+      deck[pos] = deck[i];
+      deck[i] = c;
+    }
+  }
+
+  spc_type dealTopCard()
+  {
+    if(positionInDeck < CARDS_IN_DECK)
+      return deck[positionInDeck++];
+    else
+      return deck[0];
+  }
+
+  template <unsigned N>
+  std::vector<spc_type> dealHand ()
+  {
+    std::vector<spc_type> hand;
+    for(unsigned i = 0; i < N; ++i)
+    {
+      hand.push_back(dealTopCard());
+    }
+
+    return hand;
+  }
+
 
 }; // Deck class
 
---
+++ testDeck.cxx
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include "Card.hxx"
+#include "Hand.hxx"
+#include "Deck.hxx"
+#include "PokerUtils.hxx"
+
+
+typedef std::vector<spc_type> hand;
+
+int main()
+{
+  Deck d;
+  std::vector<hand> h;
+  std::vector<Ranked_Hand> rh;
+
+  d.shuffle();
+
+  for(unsigned i = 0; i < 4; ++i)
+  {
+    h.push_back(d.dealHand<5>());
+    for(auto &c: h[i])
+      std::cout << c << ", ";
+
+    std::cout << std::endl;
+
+    rh.push_back(rankHand(h[i]));
+    std::cout << rh[i] << std::endl << std::endl;
+  }
+
+  std::cout << "==================================================" << std::endl
+	    << "Sorted"                                             << std::endl
+	    << "==================================================" << std::endl;
+
+  std::sort(rh.begin(), rh.end());
+
+  for(auto &h: rh)
+    std::cout << h << std::endl;
+
+
+
+}
+
